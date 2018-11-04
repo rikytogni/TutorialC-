eabec233c22cab7e44d29baf775549da016150a3
rikytogni <riccardo.togni2@gmail.com>
2018-11-04 20:49:49 +0000

    Test commit using VS Code

--- Box.cpp
+++ Box.cpp
@@ -6,7 +6,7 @@ using std::endl;
 CBox::CBox(double lv, double hv, double wv) :
          m_Length{lv}, m_Height{hv},m_Width{wv}
         {
-            cout << "Constructor called " << endl;
+            cout << "Constructor called !" << endl;
         }
 double CBox::volume() const
 {
